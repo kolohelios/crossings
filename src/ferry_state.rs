8fefa2537bf0f172de5d0f80f31f7a8c9509bbae
Jon Edwards <jkedwards@me.com>
2022-10-17 14:01:43 -0700

    get repositioning working

--- src/ferry_state.rs
+++ src/ferry_state.rs
@@ -3,7 +3,7 @@ use machine::*;
 machine!(
     #[derive(Clone, Debug, PartialEq)]
     enum FerryState {
-        Docked {},
+        Docked,
         Loading {
             passengers: u16,
         },
@@ -20,18 +20,33 @@ machine!(
 pub struct Load {
     pub amount: u16,
 }
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct Reposition;
+
 #[derive(Clone, Debug, PartialEq)]
 pub struct Depart;
 
+// #[derive(Clone, Debug, PartialEq)]
+// pub struct Dock;
+
 transitions!(FerryState, [
     (Docked, Load) => Loading,
+    (Docked, Reposition) => Crossing,
     (Loading, Depart) => [ Loading, Departing ]
+    // (Crossing, Dock) => Docked,
 ]);
 
 impl Docked {
     pub fn on_load(self, input: Load) -> Loading {
         Loading {
             passengers: input.amount,
+        }
+    }
+
+    pub fn on_reposition(self, _: Reposition) -> Crossing {
+        Crossing { 
+            passengers: 0
         }
     }
 }
@@ -42,6 +57,12 @@ impl Loading {
         f
     }
 }
+
+// impl Crossing {
+//     pub fn on_dock(self, _: Dock) -> Docked {
+//         FerryState::docked()
+//     }
+// }
 
 pub mod ferry_state {
     use super::*;
@@ -59,7 +80,14 @@ mod tests {
     fn test_ferry_state() {
         let mut f = ferry_state::create_ferry_state();
         assert_eq!(f, FerryState::docked());
-        f = f.on_load(Load { amount: 100 });
-        assert_eq!(f, FerryState::loading(100));
+        
+        f = f.on_reposition(Reposition {});
+        assert_eq!(f, FerryState::crossing(0));
+        
+        // f = f.on_dock(Dock);
+        // assert_eq!(f, FerryState::docked());
+        // f = f.on_load(Load { amount: 100 });
+        // assert_eq!(f, FerryState::loading(100));
+        
     }
 }
